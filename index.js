9eea15ef2eb3b49f3b7d9cfe786ba8fb0e928c56
Alaa-Sokkar <71137380+AlaaSokkar@users.noreply.github.com>
2024-11-24 03:28:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-
-async function getData(){
-    var products=await fetch(`https://fakestoreapi.com/products`);
+try{async function getData(){
+   
+    var products=await fetch(`https://ecommerce.routemisr.com/api/v1/products`);
     var allProducts=await products.json();
-    var arrayOfProduct=allProducts;
-    displayProduct(arrayOfProduct);
+    var arrayOfProduct=allProducts.data;
+     displayProduct(arrayOfProduct);
     console.log(arrayOfProduct);
+}getData();
 }
-getData();
+catch(error)
+{
+document.getElementById("rowData").innerHTML=`<h3 class="p-2 text-center">${error}</h3>`
+}
+
+
 
 var container=[];
  function displayProduct(container){
 
     cartona=``;
     for(var i=0; i< container.length; i++){
-        cartona+=`<div class="col col-md-3 my-4">
+        cartona+=`<div class="col col-md-2 my-4">
                     <div class="div-img mt-2 mb-1">
-                        <img src="${container[i].image}" alt="ssaa" srcset="">
+                        <img src="${container[i].imageCover}" alt="ssaa" srcset="">
 
                     </div>
                      
-                    <h3 class="m-0" >Rate: ${container[i].rating.rate}</h3>
+                    <h3 class="m-0" > ${container[i].title.split(" ").splice(0,2).join(" ")}</h3>
                     <h4>Price:  ${container[i].price}</h4>
                 </div>`
     }
